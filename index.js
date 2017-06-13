19eb18d5cbf601693fdde7d261cc39e10ce34f76
LV <mamboer@live.com>
2017-06-13 18:06:39 +0800

    fix tags

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const path = require('path')
 /**
- * basic generator for nust application
+ * generator for fusejs
  */
 module.exports = function (data) {
   let targetFile = path.join(this.dirTheme, 'static/data/fuse.json')
   let items = data.data.files.map((obj, idx) => {
+    let tags = obj.tags || []
+    tags = tags.map((obj1, idx1) => {
+      return obj1.title
+    })
     return {
       title: obj.title,
       desc: obj.desc,
-      tags: obj.tags,
-      author: obj.author
+      tags: tags,
+      author: obj.author,
+      route: obj.route
     }
   })
   let ret = {
